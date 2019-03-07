119bbc9a7887614841c4160f9fdd4ea08f977356
Radi <radi.rusadi.s@gmail.com>
2019-03-07 07:49:15 +0700

    Fix bug map instalation

--- android/app/src/main/java/com/react_native_boilerplate/MainApplication.java
+++ android/app/src/main/java/com/react_native_boilerplate/MainApplication.java
@@ -25,32 +25,25 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage()
+          new RNGestureHandlerPackage(),
+          new MapsPackage()
       );
     }
 
     @Override
     protected String getJSMainModuleName() {
       return "index";
-    }
-
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
-    }
-
-    @Override
-    public void onCreate() {
-      super.onCreate();
-      SoLoader.init(this, /* native exopackage */ false);
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-              new MainReactPackage(),
-              new MapsPackage()
-      );
-    }
+    }   
   };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
